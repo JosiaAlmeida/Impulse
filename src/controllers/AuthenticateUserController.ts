dc5ae2eac74499f6385a96ddf41aed1f3f55c0dc
Josia Almeida <josiaalmeida0001@gmail.com>
2021-12-22 15:49:19 +0100

    Criando conexao com base de dados e gerando um token

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -5,13 +5,16 @@ class AuthenticateUserController{
 
     async handle(req: Request, res: Response){
         //Lembrar de colocar dinamicamente
-        const code = "a65b017bdac734067f8f"
+        const code = "3ed3ab5b2fe97667d15c"
 
         const service = new AuthenticateUserService()
 
-        const result =await service.execute(code)
-
-        return res.json(result)
+        try {
+            const result =await service.execute(code)
+            return res.json(result)
+        } catch (error) {
+            throw error
+        }
     }
 
 }
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import {prismaClient} from '../prisma/'
+import {sign} from 'jsonwebtoken'
 /**
  * Receber code(string)
  * Recuperar o access_token no github
@@ -42,7 +44,47 @@ class AuthenticateUserService{
             }
         })
 
-        return response.data
+        const {login, id, avatar_url, name} = response.data
+
+        //pegando usuario existente na db
+        let user = await prismaClient.user.findFirst({
+            where: {
+                github_id: id
+            }
+        })
+
+        //Criando novo usuario
+        if(!user){
+            user = await prismaClient.user.create({
+                data:{
+                    github_id: id,
+                    login,
+                    name,
+                    avatar_url
+                }
+            })
+        }
+
+        //Criando um token
+        const token = sign(
+            {
+                user:{
+                    name: user.name,
+                    avatar_url: user.avatar_url,
+                    id: user.id
+                }
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '1d'
+            }
+        )
+
+        return {
+            token,
+            user
+        }
     }
 }
 
